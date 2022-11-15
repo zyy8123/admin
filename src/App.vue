e5de544053dabceabfe6111bbdee364ea6b8a3fd
poixao <1412634436@qq.com>
2022-11-15 21:15:39 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { generaMenu } from "./assets/js/menu";
+export default {
+  created() {
+    // 刷新页面查询用户菜单
+    // 每次刷新页面，都会去请求获取当前用户的菜单(及时生效权限)
+    if (this.$store.state.userId != null) {
+      generaMenu();
+    }
+    // 上传访客信息
+    // 刷新一次，就上传一次访客记录
+    this.axios.post("/api/report");
+  }
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: "favicon.ico",
+      favicon16: "favicon.ico",
+      appleTouchIcon: "favicon.ico",
+      maskIcon: "favicon.ico",
+      msTileImage: "favicon.ico"
+    }
+  },
+  productionSourceMap: false,
+  // 解决跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
+    disableHostCheck: true
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src"));
+  }
+};
+
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
